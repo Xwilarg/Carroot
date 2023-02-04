2a7dee5fbf24849cf8efaadc4089abcfcd58e7c4
Xwilarg <xwilarg@protonmail.com>
2023-02-04 12:33:27 +0100

    Spawn player in level

--- Assets/Scripts/Level/LevelLoader.cs
+++ Assets/Scripts/Level/LevelLoader.cs
@@ -1,13 +1,26 @@
 ﻿using GlobalGameJam2023.Menu;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 
 namespace GlobalGameJam2023.Level
 {
     public class LevelLoader : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject _player;
+
         private void Awake()
         {
+            SceneManager.sceneLoaded += (scene, mode) =>
+            {
+                if (scene != SceneManager.GetActiveScene())
+                {
+                    var spawn = GameObject.FindGameObjectsWithTag("Spawn");
+                    Assert.AreEqual(1, spawn.Length, $"There should be only 1 spawn point per level by {spawn.Length} were found");
+                    Instantiate(_player, spawn[0].transform.position, Quaternion.identity);
+                }
+            };
             SceneManager.LoadScene($"Level{LevelSelector.TargetLevel}", LoadSceneMode.Additive);
         }
     }
