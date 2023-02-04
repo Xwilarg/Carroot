a4255a0be9b8e926d990a8fede96b9d60ba40248
Xwilarg <xwilarg@protonmail.com>
2023-02-04 23:30:33 +0100

    Set max level as 3

--- Assets/Scripts/Menu/LevelSelector.cs
+++ Assets/Scripts/Menu/LevelSelector.cs
@@ -6,7 +6,7 @@ namespace GlobalGameJam2023.Menu
     public class LevelSelector : MonoBehaviour
     {
         public static int TargetLevel { private set; get; } = 1;
-        public static int LastLevel => 2;
+        public static int LastLevel => 3;
 
         public void LoadLevel(int index)
         {
