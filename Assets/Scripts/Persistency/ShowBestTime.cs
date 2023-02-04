285b559e553762800053a49d56e9c1381fe3bc43
Xwilarg <xwilarg@protonmail.com>
2023-02-04 23:01:25 +0100

    Display best time on menu

---
+++ Assets/Scripts/Persistency/ShowBestTime.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace GlobalGameJam2023.Persistency
+{
+    public class ShowBestTime : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _target;
+
+        [SerializeField]
+        private int _levelId;
+
+        private void Awake()
+        {
+            if (DataManager.Instance.SaveData.LevelData.ContainsKey(_levelId))
+            {
+                _target.text = $"{DataManager.Instance.SaveData.LevelData[_levelId].Time / 100f:0.00}";
+            }
+            else
+            {
+                _target.gameObject.SetActive(false);
+            }
+        }
+    }
+}
