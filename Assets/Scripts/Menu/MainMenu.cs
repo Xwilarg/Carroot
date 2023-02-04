6aec186614c54f6c9d16c4b3d674823fb9d10ba3
Xwilarg <xwilarg@protonmail.com>
2023-02-04 10:35:26 +0100

    Add base main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GlobalGameJam2023.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _quitButton;
+
+        private void Awake()
+        {
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                _quitButton.SetActive(false); // We can't quit a WebGL game
+            }
+        }
+
+        public void LaunchGame()
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
